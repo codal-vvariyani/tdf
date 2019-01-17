61ab5ff07aabd15b4a7983039aebba080351b543
codal-vvariyani <vvariyani@codal.com>
2019-01-17 19:27:48 +0530

    message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tdf';
+
+  topics = ['Angular','React','Vue'];
+  userModel= new User('abc', 'abc@test.com', 5556665566, 'default', 'morning', true);
+  topicHasError=true;
+
+  validateTopic(value){
+    if(value === 'default'){
+      this.topicHasError=true;
+    }
+    else{
+      this.topicHasError=false;
+    }
+  }
 }
